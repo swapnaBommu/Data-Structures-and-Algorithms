2c350a6f782a34a0de2e608873acb8c9337017eb
swapnaBommu <swapnabommu8143@gmail.com>
2023-11-16 10:49:45 +0530

    selection sort problem

---
+++ Dynamic Programming/Min cost Path problem.java
@@ -0,0 +1,11 @@
+/*
+ An integer matrix of size(M x N) has been given.  Find out the minimum cost to reach
+ the cell (0,0) to (M-1,N-1).
+
+ From a cell(i,j) you can move in 3 directions.
+ 1.(i,j+1)--->right
+ 2.(i+1,j)--->down
+ 3.(i+1)(j+1)--->diagonal
+
+ The cost of the path is defined as
+ */
---
+++ sorting problems/SelectionSort.java
@@ -0,0 +1,33 @@
+/*selection sort problem */
+package sorting problems;
+
+public class SelectionSort {
+    public static void main(String args[]){
+        int[] arr = {10,85,-5,-6,14,7,13,26};
+        selectionSort(arr);
+        printArray(arr);
+    }
+    private static void printArray(int[] arr) {
+		for(int i = 0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+    private static void selectionSort(int[] arr) {
+		int n =  arr.length;
+		for(int i =0;i<n-1;i++) {
+			int min = Integer.MAX_VALUE;
+			int minIndex = -1;
+			//finding minimum of the array
+			for(int j = i; j<n;j++) {
+				if(arr[j] < min) {
+					min = arr[j];
+					minIndex = j;
+				}
+			}
+			//swap the min value to its correct position
+			int temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
+		}	
+	}
+}
